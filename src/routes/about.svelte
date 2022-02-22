1d5ae91e09dfd6c8417fbbd67f23e7dadd84ec0a
Brage Fuglseth <brage.fuglseth@gmail.com>
2022-02-22 15:15:29 +0100

    Add some information

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,9 @@
 <h2>About</h2>
+<p>
+  Puggit is a simple tool for memorizing stuff. Just create a set, fill in what
+  you need to remember and start practicing!
+</p>
+<p>
+  The name "puggit" stems from the norwegian term "å pugge", which means "to
+  memorize".
+</p>
