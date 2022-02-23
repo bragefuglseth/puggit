b4d907c012227aa90d0711101da5519aae02de15
Brage Fuglseth <brage.fuglseth@gmail.com>
2022-02-23 23:21:11 +0100

    Add custom button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let type = 'default';
+  export let filled = false;
+</script>
+
+<button on:click|preventDefault class:filled class={type}>
+  <slot />
+</button>
+
+<style>
+  button {
+    outline: none;
+    background: none;
+    border: none;
+    padding: 0.8em;
+    border-radius: 5px;
+    font-family: 'Montserrat';
+    cursor: pointer;
+    transition: border-color 200ms, color 200ms;
+  }
+
+  button.default {
+    border: 1px solid var(--clr-text);
+    color: var(--clr-text);
+  }
+
+  button.default:hover {
+    border-color: var(--clr-text-alt);
+    color: var(--clr-text-alt);
+  }
+</style>
---
+++ src/lib/components/sets/set-card.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Card from '$lib/components/card.svelte';
+  import Button from '$lib/components/button.svelte';
+  import { createEventDispatcher } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  const dispatch = createEventDispatcher();
+
+  export let set;
+
+  let isHovered = false;
+
+  function deleteSet() {
+    dispatch('delete', {
+      id: set.id,
+    });
+  }
+</script>
+
+<a
+  href={`/set/${set.id}`}
+  on:mouseenter={() => {
+    isHovered = true;
+  }}
+  on:mouseleave={() => {
+    isHovered = false;
+  }}
+>
+  <span class="card-wrapper">
+    <Card>
+      <div class="card-content">
+        <h2>{`${set.name.slice(0, 20)}${set.name.length > 20 ? '…' : ''}`}</h2>
+        <p>
+          {set.elements.length}
+          {set.elements.length === 1 ? 'element' : 'elements'}
+        </p>
+        {#if isHovered}
+          <div class="card-options" transition:fade|local={{ duration: 200 }}>
+            <Button on:click={deleteSet}>Delete</Button>
+          </div>
+        {/if}
+      </div>
+    </Card>
+  </span>
+</a>
+
+<style>
+  .card-wrapper:hover h2 {
+    text-decoration: underline;
+  }
+
+  .card-content {
+    position: relative;
+    height: 10rem;
+  }
+
+  .card-options {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--clr-text);
+  }
+</style>
