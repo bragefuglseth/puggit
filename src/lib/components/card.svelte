73b7125f05cdc268b1a5f87b1a16cadb8ffce193
Brage Fuglseth <brage.fuglseth@gmail.com>
2022-02-23 23:21:27 +0100

    Restructure lib

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,12 +1,33 @@
-<div class="card">
+<script>
+  export let type = 'default';
+</script>
+
+<div class={type}>
   <slot />
 </div>
 
 <style>
-  .card {
-    border: 1px solid var(--clr-secondary);
+  div {
+    border: 1px solid;
     border-radius: 10px;
     padding: 1rem;
+    transition: border-color 200ms, color 200ms, background-color 200ms;
+    overflow: hidden;
+  }
+  .default {
+    border-color: var(--clr-background-alt);
     color: var(--clr-text);
+  }
+  .default:hover {
+    border-color: var(--clr-text);
+  }
+  .primary {
+    border-color: var(--clr-primary);
+    background-color: var(--clr-primary);
+    color: var(--clr-background);
+  }
+  .primary:hover {
+    border-color: var(--clr-primary-alt);
+    background-color: var(--clr-primary-alt);
   }
 </style>
---
+++ src/lib/components/sets/set-list.svelte
@@ -0,0 +1,77 @@
+<script>
+  import { flip } from 'svelte/animate';
+  import { fade } from 'svelte/transition';
+  import SetCard from '$lib/components/sets/set-card.svelte';
+  import Card from '$lib/components/card.svelte';
+
+  export let sets = [];
+
+  function deleteSet(id) {
+    console.log($sets);
+    const newSets = $sets.filter((set) => {
+      return set.id !== id;
+    });
+    sets.set(newSets);
+  }
+</script>
+
+<ul class="set-list">
+  <li>
+    <a href="/set/create">
+      <Card type="primary">
+        <div class="create-card-content">
+          <span class="create-card-icon">+</span>
+          <h3 class="create-card-text">Create set</h3>
+        </div>
+      </Card>
+    </a>
+  </li>
+  {#each $sets as set (set.id)}
+    <li out:fade|local={{ duration: 200 }} animate:flip={{ duration: 200 }}>
+      <SetCard
+        {set}
+        on:delete={(e) => {
+          deleteSet(e.detail.id);
+        }}
+      />
+    </li>
+  {/each}
+</ul>
+
+<style>
+  .create-card-content {
+    height: 10rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+  }
+
+  .create-card-text {
+    margin: 0;
+  }
+
+  .set-list {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 0.5rem;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  @media only screen and (min-width: 620px) {
+    .set-list {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media only screen and (min-width: 960px) {
+    .set-list {
+      grid-template-columns: repeat(3, calc(100% / 3));
+    }
+  }
+</style>
